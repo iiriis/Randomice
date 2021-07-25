36e2b5a98edec2282269fe80a95cc2cfe821f7cd
Deblina Chattopadhyay <62389473+Deblina28@users.noreply.github.com>
2021-07-25 16:11:31 +0530

    Added SSD and driving LEDs

--- Randomice/Randomice.ino
+++ Randomice/Randomice.ino
@@ -2,14 +2,65 @@ int dice=0;
 
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   srand(analogRead(A0));
-  DDRB|=0x3f;  
+
+  DDRB|=0x3f;
+  DDRD|=0xff;
+
+
 }
 
 long ps=0;
-void loop() {  
+void loop() {
   dice=(rand()%6)+1;
   Serial.println(dice);
+  driveLEDs(dice);
+  driveSSD(dice);
+
+  delay(1000);
+  srand(analogRead(A0));
 }
 
+void driveLEDs(byte x)
+{
+  switch(x)
+  {
+    case 1: PORTB=0x01;
+            break;
+    case 2: PORTB=0x03;
+            break;
+    case 3: PORTB=0x07;
+            break;
+    case 4: PORTB=0x0f;
+            break;
+    case 5: PORTB=0x1f;
+            break;
+    case 6: PORTB=0x3f;
+            break;
+    default : PORTB|=0x00;
+              break;
+  }
+}
+
+void driveSSD(byte x)
+{
+  switch(x)
+  {
+    case 1: PORTD=0x06;
+            break;
+    case 2: PORTD=0x5b;
+            break;
+    case 3: PORTD=0x4f;
+            break;
+    case 4: PORTD=0x66;
+            break;
+    case 5: PORTD=0x6d;
+            break;
+    case 6: PORTD=0x7d;
+            break;
+    default : PORTD|=0x00;
+              break;
+  }
+
+}
