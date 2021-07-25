b189f69168e0330990b6218bee815bf9ac986b2d
iiriis <55133414+iiriis@users.noreply.github.com>
2021-07-25 15:42:42 +0530

    Printing the random Nos 1 to 6.

---
+++ Randomice/Randomice.ino
@@ -0,0 +1,15 @@
+int dice=0;
+
+void setup() 
+{
+  Serial.begin(115200);
+  srand(analogRead(A0));
+  DDRB|=0x3f;  
+}
+
+long ps=0;
+void loop() {  
+  dice=(rand()%6)+1;
+  Serial.println(dice);
+}
+
